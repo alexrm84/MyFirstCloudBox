096e2e8366ebc03e54329c04f5ce6742f5bf18a5
alexrm84 <alexrm84@gmail.com>
2019-04-23 21:54:12 +0300

    Добавлено движение по каталогу вглубь и обратно

--- client/src/main/java/alexrm84/myFirstCloudBox/client/Controller.java
+++ client/src/main/java/alexrm84/myFirstCloudBox/client/Controller.java
@@ -2,9 +2,6 @@ package alexrm84.myFirstCloudBox.client;
 
 import alexrm84.myFirstCloudBox.common.AbstractMessage;
 import alexrm84.myFirstCloudBox.common.FileMessage;
-import io.netty.handler.codec.serialization.ObjectDecoderInputStream;
-import io.netty.handler.codec.serialization.ObjectEncoderOutputStream;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,12 +9,14 @@ import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import lombok.Data;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.nio.file.*;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
+
+import static java.nio.file.Files.newDirectoryStream;
 
 @Data
 public class Controller implements Initializable {
@@ -33,6 +32,7 @@ public class Controller implements Initializable {
 
     private boolean authenticated;
     private String nickname;
+    private String currentClientPath, currentServerPath;
 
     public void setAuthenticated(boolean authenticated) {
         this.authenticated = authenticated;
@@ -42,28 +42,19 @@ public class Controller implements Initializable {
         hbControlPanel.setManaged(authenticated);
         if (!authenticated) {
             nickname = "";
+            lvClientFilesList.getItems().clear();
+            lvServerFilesList.getItems().clear();
+        }else {
+            refreshClientFilesList(currentClientPath);
+            work();
         }
-        lvClientFilesList.setOnMouseClicked(event -> {
-            if (event.getClickCount() == 2) {
-                String filename = lvClientFilesList.getSelectionModel().getSelectedItem();
-                tfFilename.setText(filename);
-                tfFilename.requestFocus();
-                tfFilename.selectEnd();
-            }
-        });
-        lvServerFilesList.setOnMouseClicked(event -> {
-            if (event.getClickCount() == 2) {
-                String filename = lvServerFilesList.getSelectionModel().getSelectedItem();
-                tfFilename.setText(filename);
-                tfFilename.requestFocus();
-                tfFilename.selectEnd();
-            }
-        });
+
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setAuthenticated(false);
+        currentClientPath = "client_storage";
         Network.start();
         Thread thread = new Thread(()->{
             try {
@@ -72,7 +63,7 @@ public class Controller implements Initializable {
                     if (abstractMessage instanceof FileMessage){
                         FileMessage fileMessage = (FileMessage)abstractMessage;
                         Files.write(Paths.get("client_storage/" + fileMessage.getFilename()),fileMessage.getData(), StandardOpenOption.CREATE);
-                        refreshLocalFilesList();
+//                        refreshFilesList("client_storage");
                     }
                 }
             }catch (ClassNotFoundException | IOException e){
@@ -83,28 +74,82 @@ public class Controller implements Initializable {
         });
         thread.setDaemon(true);
         thread.start();
-        refreshLocalFilesList();
     }
 
-    public void refreshLocalFilesList() {
-        if (Platform.isFxApplicationThread()) {
-            try {
-                lvClientFilesList.getItems().clear();
-                Files.list(Paths.get("client_storage")).map(p -> p.getFileName().toString()).forEach(o -> lvClientFilesList.getItems().add(o));
-            } catch (IOException e) {
-                e.printStackTrace();
+    public void work(){
+        lvClientFilesList.getItems().clear();
+        refreshClientFilesList(currentClientPath);
+        lvClientFilesList.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 1) {
+                String filename = lvClientFilesList.getSelectionModel().getSelectedItem();
+                tfFilename.setText(filename);
+                tfFilename.requestFocus();
+                tfFilename.selectEnd();
             }
-        } else {
-            Platform.runLater(() -> {
-                try {
-                    lvClientFilesList.getItems().clear();
-                    Files.list(Paths.get("client_storage")).map(p -> p.getFileName().toString()).forEach(o -> lvClientFilesList.getItems().add(o));
-                } catch (IOException e) {
-                    e.printStackTrace();
+            if (event.getClickCount() == 2) {
+                String filename = lvClientFilesList.getSelectionModel().getSelectedItem();
+                if (filename.equals("[..]")){
+                    refreshClientFilesList(Paths.get(currentClientPath).getParent().toString());
                 }
-            });
+                if (Files.isDirectory(Paths.get(currentClientPath+"/"+filename),LinkOption.NOFOLLOW_LINKS)){
+                    currentClientPath+="/"+filename;
+                    refreshClientFilesList(currentClientPath);
+                }
+
+            }
+        });
+    }
+
+//    public void refreshLocalFilesList() {
+//        if (Platform.isFxApplicationThread()) {
+//            try {
+//                lvClientFilesList.getItems().clear();
+//                Files.list(Paths.get("client_storage")).map(p -> p.getFileName().toString()).forEach(o -> lvClientFilesList.getItems().add(o));
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        } else {
+//            Platform.runLater(() -> {
+//                try {
+//                    lvClientFilesList.getItems().clear();
+//                    Files.list(Paths.get("client_storage")).map(p -> p.getFileName().toString()).forEach(o -> lvClientFilesList.getItems().add(o));
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//        }
+//    }
+
+    public void refreshClientFilesList(String directoryName){
+        lvClientFilesList.getItems().clear();
+        if (!currentClientPath.equals("client_storage")){
+            lvClientFilesList.getItems().add("[..]");
+        }
+        try {
+            for (Path p:newDirectoryStream(Paths.get(directoryName))) {
+                if (Files.isDirectory(p, LinkOption.NOFOLLOW_LINKS)){
+                    lvClientFilesList.getItems().add(p.getName(p.getNameCount()-1).toString());
+                }else {
+                    lvClientFilesList.getItems().add(p.getFileName().toString());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+    public void getDirectoryContentsToCopy(String directoryName, LinkedList<File> files) {
+        File directory = new File(directoryName);
+        File[] fList = directory.listFiles();
+        for (File file : fList) {
+            if (file.isFile()) {
+                files.add(file);
+            } else if (file.isDirectory()) {
+                getDirectoryContentsToCopy(file.getAbsolutePath(), files);
+            }
+        }
+    }
+
 
     public void authorization(ActionEvent actionEvent) {
         setAuthenticated(true);
@@ -122,6 +167,6 @@ public class Controller implements Initializable {
     }
 
     public void receive(ActionEvent actionEvent) {
-        refreshLocalFilesList();
+        refreshClientFilesList(currentClientPath);
     }
 }
